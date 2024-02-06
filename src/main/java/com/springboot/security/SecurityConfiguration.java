31aaafc1913be08ed76349b0eac6051499ae3cf6
christallobo <christal1803@gmail.com>
2024-02-06 10:38:15 +0530

    Configure Spring Security Authorization

--- src/main/java/com/springboot/security/SecurityConfiguration.java
+++ src/main/java/com/springboot/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.springboot.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -12,7 +13,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        // setting the configuration on the auth object
+        // setting the configuration on the auth object(authentication)
          auth.inMemoryAuthentication()
                  .withUser("abc")
                  .password("abc123")
@@ -28,5 +29,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     public PasswordEncoder getPasswordEncoder(){
        // It does nothing but keeps the clear text
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/**").hasRole("ADMIN")
+//                .and().formLogin();
+
+        //Role based authorization
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("USER","ADMIN")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
     }
 }
